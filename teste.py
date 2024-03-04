f004d19873975d60bcaab10705763b0a85800c86
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2024-03-04 13:00:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,16 @@
 import re
+
+
 def transformacao_data(date_str):
+
     data = re.compile(r'(\d{4})-(\d{2})-(\d{2})')
+
     data_brasileira = data.sub(r'\3/\2/\1', date_str)
+
     return data_brasileira
+
 date = "2021-11-30"
+
 mudanca_data = transformacao_data(date)
+
 print(mudanca_data)  
